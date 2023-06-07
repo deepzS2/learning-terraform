8f44483e6fa165106ff919d5288f6a23d2c4bdbc
deepzS2 <alanbackman10@hotmail.com>
2023-06-07 13:27:58 -0300

    feat: azure vm with vnet remote state

---
+++ azure-vm-with-vnet-remote-state/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.59.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "remote-state"
+    storage_account_name = "deepzs2remotestate"
+    container_name       = "remote-state"
+    key                  = "azure-vm/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
+
+data "terraform_remote_state" "vnet" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = "remote-state"
+    storage_account_name = "deepzs2remotestate"
+    container_name       = "remote-state"
+    key                  = "azure-vnet/terraform.tfstate"
+  }
+}
