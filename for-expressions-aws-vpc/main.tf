a5d017a6d3bd845845aec4639bf020f013f01408
deepzS2 <alanbackman10@hotmail.com>
2023-06-12 21:23:59 -0300

    feat: for expressions

---
+++ for-expressions-aws-vpc/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "deepzs2-terraform-remote-state"
+    key    = "aws-vpc-for-expressions/terraform.lfstate"
+    region = "sa-east-1"
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+
+  default_tags {
+    tags = {
+      owner      = "deepzS2"
+      managed-by = "terraform"
+    }
+  }
+}
